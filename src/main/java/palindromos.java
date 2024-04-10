6cfa9cdb3ec6158c70713c82db9764e30d5eeb78
Toritou <l.toro05@ufromail.cl>
2024-04-09 22:30:05 -0400

    creado el main con algunos comentarios

---
+++ src/main/java/palindromos.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class palindromos {
+    public static void main(String[] args) {
+        /* para leer la entrada de texto */
+        Scanner sc = new Scanner(System.in);
+        System.out.println(" Ingrese la palabra a comprobar: ");
+        /* se lee la entrada de texto */
+        String palabra = sc.nextLine();
+
+    }
+}
